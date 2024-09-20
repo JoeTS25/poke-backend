c8ce59bb1f2a5af9b9871f2297ddf33df66127e2
Joe Scibilia <joescibilia@Joes-MacBook-Pro.local>
2024-09-20 10:11:40 -0400

    first commit

---
+++ models/_testCommon.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcrypt");
+
+const db = require("../db.js");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const { createToken } = require("../helpers/tokens");
+
+async function commonBeforeAll() {
+    await db.query("DELETE FROM users");
+
+    await db.query(`
+        INSERT INTO users(username,
+                          password,
+                          email)
+        VALUES ('u1', $1, 'u1@email.com'),
+               ('u2', $2, 'u2@email.com')
+        RETURNING username`,
+      [
+        await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
+        await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
+      ]);
+}
+
+async function commonBeforeEach() {
+    await db.query("BEGIN");
+  }
+  
+  async function commonAfterEach() {
+    await db.query("ROLLBACK");
+  }
+  
+  async function commonAfterAll() {
+    await db.end();
+  }
+
+  const u1Token = createToken({ username: "u1", isAdmin: false });
+  const u2Token = createToken({ username: "u2", isAdmin: false });
+  
+  
+  module.exports = {
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll,
+    u1Token,
+    u2Token,
+  };
